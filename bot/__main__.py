2327163c751cb66ae9a79bad0cb7f0977016c687
ale.baranov <ale.baranov@yadro.com>
2025-11-25 17:03:11 +0300

    task9

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,7 @@
 from bot.dispatcher import Dispatcher
 from bot.handlers import get_handlers
-from bot.sqlite_database.storage_sqlite import StorageSqlite
+
+from bot.postgres_database.storage_postgres import StoragePostgres
 from bot.tg_bot_realization.telegram_bot import MessengerTelegram
 from bot.domain.storage import Storage
 from bot.domain.messenger import Messenger
@@ -9,7 +10,7 @@ from bot.long_polling import start_long_polling
 
 if __name__ == "__main__":
     try:
-        storage: Storage = StorageSqlite()
+        storage: Storage = StoragePostgres()
         messenger: Messenger = MessengerTelegram()
         dispatcher = Dispatcher(storage, messenger)
         dispatcher.add_handlers(*get_handlers())
